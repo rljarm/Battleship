4bba8a7b8dc04ce86dc0c54d0d013999ae28f180
Dick <rljarm@gmail.com>
2015-04-18 14:24:10 -0700

    added logins and firebase

--- client/index.js
+++ client/index.js
@@ -1,2 +1,72 @@
+/* global Firebase:true */
+
 'use strict';
-console.log('apollo -> online');
+
+var root, users, battleships, myKey;
+
+$(document).ready(init);
+
+function init(){
+  root = new Firebase('https://dicks-battleship.firebaseio.com/');
+  users = root.child('users');
+  battleships = root.child('battleships');
+  $('#create-user').click(createUser);
+  $('#login-user').click(loginUser);
+  $('#logout-user').click(logoutUser);
+  $('#start-user').click(startUser);
+}
+
+function logoutUser(){
+  root.unauth();
+  myKey = null;
+  $('#characters > tbody > tr.active').removeClass('active');
+}
+
+function loginUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  root.authWithPassword({
+    email    : email,
+    password : password
+  }, function(error){
+    if(error){
+      console.log('Error logging in:', error);
+    }
+  });
+}
+
+function createUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  root.createUser({
+    email    : email,
+    password : password
+  }, function(error){
+    if(error){
+      console.log('Error creating user:', error);
+    }
+  });
+}
+
+function startUser(){
+  var x = Math.floor(Math.random() * 10);
+  var y = Math.floor(Math.random() * 10);
+  battleships.child(myKey).update({x:x, y:y});
+}
+
+function characterAdded(snapshot){
+  var character = snapshot.val();
+  var myUid = root.getAuth() ? root.getAuth().uid : '';
+  var active = '';
+
+  if(myUid === character.uid){
+    myKey = snapshot.key();
+    myCharacter = snapshot.val();
+    active = 'active';
+  }
+
+  var tr = '<tr class="'+active+'"><td>'+character.handle+'</td><td><img src="'+character.avatar+'"></td></tr>';
+  $('#characters > tbody').append(tr);
+}
